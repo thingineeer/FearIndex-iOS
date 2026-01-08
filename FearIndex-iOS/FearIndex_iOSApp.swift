4a5d815cfb28e9f1ade574c083722a9912d7be25
thingineeer <dlaudwls1203@gmail.com>
2026-01-08 09:58:33 +0900

    Initial Commit

---
+++ FearIndex-iOS/FearIndex_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FearIndex_iOSApp.swift
+//  FearIndex-iOS
+//
+//  Created by 이명진 on 1/8/26.
+//
+
+import SwiftUI
+
+@main
+struct FearIndex_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FearIndex-iOSTests/FearIndex_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  FearIndex_iOSTests.swift
+//  FearIndex-iOSTests
+//
+//  Created by 이명진 on 1/8/26.
+//
+
+import Testing
+@testable import FearIndex_iOS
+
+struct FearIndex_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
